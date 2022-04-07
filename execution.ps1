11e2a14aa3326443fb3ea9d7eb13ee2ad319e772
danijeljw-rpc <danijel@repasscloud.com>
2022-04-07 16:39:32 +1000

    add download test to script

--- execution.ps1
+++ execution.ps1
@@ -21,8 +21,6 @@ $url
 $iwrObject = Invoke-WebRequest -Uri $url -UserAgent $userAgent -UseBasicParsing
 
 
-
+$outFile = "${PSScriptRoot}\test-download.dl"
 [System.String]$cabfile = (((($iwrObject.Links | Where-Object -FilterScript {$_ -match '.*Download Now.*'}).outerHTML | Select-Object -First 1) -replace '^.*href="','') -replace '".*','') # -replace '%20',''
-$cabfile
-#$cabfile -like 'https://downloads.dell.com/FOLDER08160654M/1/Latitude%207330%20Rugged%20Extreme-win10-A01-4259W.CAB'
-#Invoke-WebRequest -Uri $cabfile -UseBasicParsing -UserAgent $userAgent -ContentType 'application/zip' -OutFile $outFile -ErrorAction Stop
+Invoke-WebRequest -Uri $cabfile -UseBasicParsing -UserAgent $userAgent -ContentType 'application/zip' -OutFile $outFile -ErrorAction Stop
